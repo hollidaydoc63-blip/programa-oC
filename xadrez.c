bef310869e680bca36392269994db7bd2f8f9ce6
hollidaydoc63-blip <hollidaydoc63@gmail.com>
2026-05-06 16:27:51 +0000

    // Jogo de xadrez utilizando Estruturas de loops. // Estruturas de repetições como while, do-while e for.

---
+++ xadrez.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    printf("### Bem-vindos ao Jogo de Tabuleiro: Xadrez ###\n");
+    // A seguir terá demonstrações práticas de movimentações de peças do xadrez utilizando for, while, e do-while
+    
+    // Mover a Torre cinco casas para a direita
+    printf("*** Torre se movimentará para a direita cinco casas ***\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n"); // Imprime a direção na qual a sua peça, que no caso do for foi escolhido Torre, irá se movimentar
+    }
+ 
+    printf("*** Bispo se movimentará cinco casas na diagonal para cima à direita ***\n");
+    int i = 0;
+    // Mover o Bispo cinco casas na diagonal para cima à direita, utilizando o while para executar o movimento
+    while (i < 5) 
+    {
+        printf("Cima, Direita\n");
+        i++;
+    }
+    i = 0;
+    printf("*** Rainha se movimetará oito casas para a esquerda ***\n");
+    // Mover a Rainha oito casas para a esquerda utilizando o do-wihile
+    do {
+        printf("Esquerda\n");
+        i++;
+    } while (i < 8);
+
+    return 0;
+
+}
